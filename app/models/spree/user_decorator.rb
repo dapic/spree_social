4af6432ae58303d746c6b9e247c148bc24a181a4
Stanley Yeh <stanley.yeh@gmail.com>
2015-01-03 17:16:02 +0800

    reversed previous commit which was an attempt to generate an email address from info provided by some login service providers. it wasn't working well as it could produce invalid email addressed as it relied on "nickname" fields which could be, and often are, in Chinese.

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,17 +4,8 @@ Spree.user_class.class_eval do
   devise :omniauthable
 
   def apply_omniauth(omniauth)
-    if email.blank?
-      case omniauth['provider']
-      when 'wechat'
-        self.email = "#{omniauth['info']['nickname']}@mail.weixin.qq.com"
-      when 'qq'
-        self.email = "#{omniauth['info']['nickname']}@mail.qqconnect.qq.com"
-      when 'weibo'
-        self.email = "#{omniauth['info']['nickname']}@mail.weibo.com"
-      else
-        self.email = omniauth['info']['email'] if omniauth['info']['email'].present?
-      end
+    if ["facebook", 'google_oauth2'].include? omniauth['provider']
+      self.email = omniauth['info']['email'] if email.blank?
     end
     user_authentications.build(provider: omniauth['provider'], uid: omniauth['uid'])
   end
